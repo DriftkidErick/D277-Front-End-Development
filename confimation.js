369bb696dfee37de592e915c05111906a2a31ab8
DriftkidErick <94418693+DriftkidErick@users.noreply.github.com>
2024-07-10 20:28:09 -0400

    Fixed JavaScript reload

--- confimation.js
+++ confimation.js
@@ -11,6 +11,11 @@ document.addEventListener("DOMContentLoaded", function () {
     else
     {
         alert("Thank you for you submission! We will get back to you as soon as possible.");
+
+        setTimeout(function() {
+          location.reload(); // Refresh the page after a short delay
+        }, 1000); // Adjust the delay time (in milliseconds) as needed
+        
     }
     
   });
